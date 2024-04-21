abe89a974074b5692d1b272fab02e76105b04dd5
Altamash Ali <altamashaali@gmail.com>
2024-04-21 18:10:00 -0400

    Update page text

--- pages/app/likes.js
+++ pages/app/likes.js
@@ -2,6 +2,9 @@ export default function LikesPage() {
   return (
     <div>
       <div class="title has-text-white has-text-centered">My Likes</div>
+      <div class="subtitle has-text-white has-text-centered">
+        Pitches you've reacted to.
+      </div>
       <div class="hero-body columns is-vcentered is-centered">
         <div className="column has-text-centered">
           <p>Column 1</p>
--- pages/app/pitch.js
+++ pages/app/pitch.js
@@ -2,6 +2,9 @@ export default function PitchPage() {
   return (
     <div>
       <div class="title has-text-white has-text-centered">My Pitches</div>
+      <div class="subtitle has-text-white has-text-centered">
+        Create new pitches or view old ones.
+      </div>
       <div class="hero-body columns is-vcentered is-centered">
         <div className="column has-text-centered">
           <p>Column 1</p>

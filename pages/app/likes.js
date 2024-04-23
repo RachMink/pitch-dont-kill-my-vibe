52542bdffa91a04580e3c8b1677ccb86babe25e7
Altamash Ali <altamashaali@gmail.com>
2024-04-23 12:07:09 -0400

    Get liked and disliked pitches

--- pages/app/likes.js
+++ pages/app/likes.js
@@ -1,40 +1,28 @@
 import PitchCard from "@/components/PitchCard";
+import { useState, useEffect } from "react";
+import * as db from "../../database";
 
-export default function LikesPage() {
-  let sampleLikes = [
-    {
-      title: "This is a test",
-      description: "This is the body",
-      date: "01/02/2024",
-      score: "6",
-    },
-    {
-      title: "This is a test",
-      description: "This is the body",
-      date: "01/02/2024",
-      score: "-1",
-    },
-  ];
-  let sampleDislikes = [
-    {
-      title: "This is a test",
-      description: "This is the body",
-      date: "01/02/2024",
-      score: "6",
-    },
-    {
-      title: "This is a test",
-      description: "This is the body",
-      date: "01/02/2024",
-      score: "-1",
-    },
-    {
-      title: "This is a test",
-      description: "This is the body",
-      date: "01/02/2024",
-      score: "-1",
-    },
-  ];
+export default function LikesPage(props) {
+  const [likedPitches, setLikedPitches] = useState([]);
+  const [dislikedPitches, setDislikedPitches] = useState([]);
+
+  const getPitches = async () => {
+    const allPitches = await db.getAllPitches();
+
+    const onlyLikedPitches = allPitches.filter((pitch) =>
+      pitch.likes.includes(props.user.email)
+    );
+    const onlyDislikedPitches = allPitches.filter((pitch) =>
+      pitch.dislikes.includes(props.user.email)
+    );
+    setLikedPitches(onlyLikedPitches);
+    setDislikedPitches(onlyDislikedPitches);
+  };
+
+  useEffect(() => {
+    getPitches();
+  }, []);
+
   return (
     <div>
       <div className="title has-text-white has-text-centered mt-6">
@@ -48,17 +36,29 @@ export default function LikesPage() {
           <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
             Liked Pitches
           </p>
-          {sampleLikes.map((samplePitch, index) => (
-            <PitchCard samplePitch={samplePitch} key={index} />
-          ))}
+          {likedPitches.length > 0 ? (
+            likedPitches.map((pitch, index) => (
+              <PitchCard pitch={pitch} key={index} getPitches={getPitches} />
+            ))
+          ) : (
+            <p className="mt-6 subtitle has-text-white">
+              There are no liked pitches to display.
+            </p>
+          )}
         </div>
         <div className="column has-text-centered mt-2 ml-6 mr-6 mb-6">
           <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
             Disliked Pitches
           </p>
-          {sampleDislikes.map((samplePitch, index) => (
-            <PitchCard samplePitch={samplePitch} key={index} />
-          ))}{" "}
+          {dislikedPitches.length > 0 ? (
+            dislikedPitches.map((pitch, index) => (
+              <PitchCard pitch={pitch} key={index} getPitches={getPitches} />
+            ))
+          ) : (
+            <p className="mt-6 subtitle has-text-white">
+              There are no disliked pitches to display.
+            </p>
+          )}{" "}
         </div>
       </div>
     </div>

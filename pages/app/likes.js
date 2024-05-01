ab89fca04fcf13fc8f3b52dd075f74152b450d50
Rachel-Minkowitz <rhminkowitz@gmail.com>
2024-05-01 16:43:12 -0400

    change color for hourglass loader

--- pages/app/likes.js
+++ pages/app/likes.js
@@ -59,7 +59,7 @@ export default function LikesPage(props) {
                 ariaLabel="hourglass-loading"
                 wrapperStyle={{}}
                 wrapperClass=""
-                colors={["#306cce", "#72a1ed"]}
+                colors={["#ffffff", "#f5c984"]}
               />
             </div>
           </div>

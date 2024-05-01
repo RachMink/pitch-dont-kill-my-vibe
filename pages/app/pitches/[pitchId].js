6410265114e76215bf80d4ad4435c55370bb68e1
Rachel-Minkowitz <rhminkowitz@gmail.com>
2024-05-01 17:06:40 -0400

    add laoding spinner to pitch specific page

--- pages/app/pitches/[pitchId].js
+++ pages/app/pitches/[pitchId].js
@@ -4,13 +4,16 @@ import CommentCard from "@/components/CommentCard";
 import * as db from "../../../database";
 import { useState, useEffect } from "react";
 import Link from "next/link";
+import { Hourglass } from "react-loader-spinner";
 
 export default function PitchSpecificPage(props) {
   const router = useRouter();
   const [currentPitch, setCurrentPitch] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   let currentPitchId;
 
   const getCurrentPitch = async () => {
+    setIsLoading(true);
     currentPitchId = currentPitchId
       ? currentPitchId
       : localStorage.getItem("storedPitchId");
@@ -19,6 +22,7 @@ export default function PitchSpecificPage(props) {
 
     current.comments.sort((a, b) => b.commentDate - a.commentDate);
     setCurrentPitch(current);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -48,74 +52,92 @@ export default function PitchSpecificPage(props) {
   };
 
   return (
-    <div>
-      <div className="title has-text-white has-text-centered mt-6">
-        Viewing <i>{currentPitch.pitchTitle}</i>
-      </div>
-      {props.userType === "Pitcher" && (
-        <div className="has-text-centered">
-          <Link href="/app/pitches">
-            <button className="button">View All Pitches</button>
-          </Link>
-        </div>
-      )}
-      {props.userType === "Venture Capital" && (
-        <div className="has-text-centered">
-          <Link href="/app/likes">
-            <button className="button">Back To Your Likes</button>
-          </Link>
-        </div>
-      )}
-
-      <div className="columns m-5">
-        <div
-          className={`column has-text-centered ${props.userType === "Venture Capital" ? "is-three-fifths" : "is-full"}`}
-        >
-          <p className="is-size-4 has-text-white has-text-weight-semibold	pb-2">
-            Highlighted Pitch
-          </p>
-          <PitchCard
-            pitch={currentPitch}
-            viewOnly={true}
-            userType={props.userType}
-          ></PitchCard>
-          <p className="is-size-4 has-text-white has-text-weight-semibold mt-4 pb-2">
-            Comments
-          </p>
-          {currentPitch.comments && currentPitch.comments.length > 0 ? (
-            currentPitch.comments.map((comment) => (
-              <CommentCard
-                comment={comment}
-                user={props.user}
-                userType={props.userType}
-                pitchId={router.query.pitchId}
-                getCurrentPitch={getCurrentPitch}
-              />
-            ))
-          ) : (
-            <p>There are no comments to display.</p>
-          )}
-        </div>
-        {props.userType === "Venture Capital" && (
-          <div className="column has-text-centered">
-            <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
-              Add a comment
-            </p>
-            <form onSubmit={(e) => onSubmit(e, currentPitch.id)}>
-              <textarea
-                className="textarea is-medium control mt-2"
-                type="text"
-                placeholder="Enter comment"
-                name="pitch-comment"
-              />
-              <div className="pt-2"></div>
-              <div className="has-text-centered pt-4 pb-4 control">
-                <button className="button is-primary">Comment</button>
-              </div>
-            </form>
+    <>
+      {isLoading ? ( // Render Spinner if loading, else render content
+        <div className="columns mt-4 ml-4 mr-4 mb-6">
+          <div className="column has-text-centered mt-2 ml-5 mr-5 mb-6">
+            <Hourglass
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="hourglass-loading"
+              wrapperStyle={{}}
+              wrapperClass=""
+              colors={["#ffffff", "#f5c984"]}
+            />
           </div>
-        )}
-      </div>
-    </div>
+        </div>
+      ) : (
+        <div>
+          <div className="title has-text-white has-text-centered mt-6">
+            Viewing <i>{currentPitch.pitchTitle}</i>
+          </div>
+          {props.userType === "Pitcher" && (
+            <div className="has-text-centered">
+              <Link href="/app/pitches">
+                <button className="button">View All Pitches</button>
+              </Link>
+            </div>
+          )}
+          {props.userType === "Venture Capital" && (
+            <div className="has-text-centered">
+              <Link href="/app/likes">
+                <button className="button">Back To Your Likes</button>
+              </Link>
+            </div>
+          )}
+
+          <div className="columns m-5">
+            <div
+              className={`column has-text-centered ${props.userType === "Venture Capital" ? "is-three-fifths" : "is-full"}`}
+            >
+              <p className="is-size-4 has-text-white has-text-weight-semibold	pb-2">
+                Highlighted Pitch
+              </p>
+              <PitchCard
+                pitch={currentPitch}
+                viewOnly={true}
+                userType={props.userType}
+              ></PitchCard>
+              <p className="is-size-4 has-text-white has-text-weight-semibold mt-4 pb-2">
+                Comments
+              </p>
+              {currentPitch.comments && currentPitch.comments.length > 0 ? (
+                currentPitch.comments.map((comment) => (
+                  <CommentCard
+                    comment={comment}
+                    user={props.user}
+                    userType={props.userType}
+                    pitchId={router.query.pitchId}
+                    getCurrentPitch={getCurrentPitch}
+                  />
+                ))
+              ) : (
+                <p>There are no comments to display.</p>
+              )}
+            </div>
+            {props.userType === "Venture Capital" && (
+              <div className="column has-text-centered">
+                <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
+                  Add a comment
+                </p>
+                <form onSubmit={(e) => onSubmit(e, currentPitch.id)}>
+                  <textarea
+                    className="textarea is-medium control mt-2"
+                    type="text"
+                    placeholder="Enter comment"
+                    name="pitch-comment"
+                  />
+                  <div className="pt-2"></div>
+                  <div className="has-text-centered pt-4 pb-4 control">
+                    <button className="button is-primary">Comment</button>
+                  </div>
+                </form>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }

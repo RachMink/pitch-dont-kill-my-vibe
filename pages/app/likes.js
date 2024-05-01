1f530c17364235c2e95582956375826ffa8b183b
Rachel-Minkowitz <rhminkowitz@gmail.com>
2024-05-01 16:12:49 -0400

    add loader on likes page - lmk styling thoughts

--- pages/app/likes.js
+++ pages/app/likes.js
@@ -1,13 +1,16 @@
 import PitchCard from "@/components/PitchCard";
 import { useState, useEffect } from "react";
 import * as db from "../../database";
+import { Hourglass } from "react-loader-spinner";
 
 export default function LikesPage(props) {
   const [likedPitches, setLikedPitches] = useState([]);
   const [dislikedPitches, setDislikedPitches] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   let currentEmail;
 
   const getPitches = async () => {
+    setIsLoading(true);
     const allPitches = await db.getAllPitches();
 
     currentEmail = currentEmail
@@ -24,6 +27,7 @@ export default function LikesPage(props) {
 
     setLikedPitches(onlyLikedPitches);
     setDislikedPitches(onlyDislikedPitches);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -38,41 +42,67 @@ export default function LikesPage(props) {
 
   return (
     <div>
-      <div className="title has-text-white has-text-centered mt-6">
-        My Likes
-      </div>
-      <div className="subtitle has-text-white has-text-centered">
-        Click on pitch to add, edit, delete, or view comments.
-      </div>
-      <div className="columns mt-4 ml-4 mr-4 mb-6">
-        <div className="column has-text-centered mt-2 ml-5 mr-5 mb-6">
-          <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
-            Liked Pitches
-          </p>
-          {likedPitches.length > 0 ? (
-            likedPitches.map((pitch, index) => (
-              <PitchCard pitch={pitch} key={index} getPitches={getPitches} />
-            ))
-          ) : (
-            <p className="mt-6 subtitle has-text-white">
-              There are no liked pitches to display.
-            </p>
-          )}
+      <div>
+        <div className="title has-text-white has-text-centered mt-6">
+          My Likes
         </div>
-        <div className="column has-text-centered mt-2 ml-5 mr-5 mb-6">
-          <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
-            Disliked Pitches
-          </p>
-          {dislikedPitches.length > 0 ? (
-            dislikedPitches.map((pitch, index) => (
-              <PitchCard pitch={pitch} key={index} getPitches={getPitches} />
-            ))
-          ) : (
-            <p className="mt-6 subtitle has-text-white">
-              There are no disliked pitches to display.
-            </p>
-          )}{" "}
+        <div className="subtitle has-text-white has-text-centered">
+          Click on pitch to add, edit, delete, or view comments.
         </div>
+        {isLoading ? ( // Render Spinner if loading, else render content
+          <div className="columns mt-4 ml-4 mr-4 mb-6">
+            <div className="column has-text-centered mt-2 ml-5 mr-5 mb-6">
+              <Hourglass
+                visible={true}
+                height="80"
+                width="80"
+                ariaLabel="hourglass-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                colors={["#306cce", "#72a1ed"]}
+              />
+            </div>
+          </div>
+        ) : (
+          <div className="columns mt-4 ml-4 mr-4 mb-6">
+            <div className="column has-text-centered mt-2 ml-5 mr-5 mb-6">
+              <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
+                Liked Pitches
+              </p>
+              {likedPitches.length > 0 ? (
+                likedPitches.map((pitch, index) => (
+                  <PitchCard
+                    pitch={pitch}
+                    key={index}
+                    getPitches={getPitches}
+                  />
+                ))
+              ) : (
+                <p className="mt-6 subtitle has-text-white">
+                  There are no liked pitches to display.
+                </p>
+              )}
+            </div>
+            <div className="column has-text-centered mt-2 ml-5 mr-5 mb-6">
+              <p className="is-size-4 has-text-white has-text-weight-semibold pb-2">
+                Disliked Pitches
+              </p>
+              {dislikedPitches.length > 0 ? (
+                dislikedPitches.map((pitch, index) => (
+                  <PitchCard
+                    pitch={pitch}
+                    key={index}
+                    getPitches={getPitches}
+                  />
+                ))
+              ) : (
+                <p className="mt-6 subtitle has-text-white">
+                  There are no disliked pitches to display.
+                </p>
+              )}{" "}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );

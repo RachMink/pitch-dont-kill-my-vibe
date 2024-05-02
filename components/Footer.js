ee96f44a659e546dc01d61e817793b4400fa7bb3
Rachel-Minkowitz <rhminkowitz@gmail.com>
2024-05-02 12:42:31 -0400

    update footer color

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <div className="hero-footer has-background-success-light	">
+    <div className="hero-footer has-background-light">
       <div className="has-text-centered has-text-black">
         Project by Altamash Ali & Rachel Minkowitz
       </div>
